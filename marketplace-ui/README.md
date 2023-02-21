75ece41fe7205623d73db0b9840aa8e63da6eaab
franjpr <franpr.dev@protonmail.com>
2023-02-21 23:19:45 +0000

    feat: benefits

--- marketplace-ui/README.md
+++ marketplace-ui/README.md
@@ -4,5 +4,5 @@
    3. Can remove them from store
 2. Page where user can see active nfts ✅
    1. Can buy nfts
-   2. Can see/withdraw benefis
+   2. Can see/withdraw benefis ✅
 3. Navbar where user can see balance and connect / disconnect btn ✅
--- marketplace-ui/src/pages/index.tsx
+++ marketplace-ui/src/pages/index.tsx
@@ -1,12 +1,49 @@
 import styles from '@/styles/Home.module.css';
-import { useMoralis } from 'react-moralis';
+import { useMoralis, useWeb3Contract } from 'react-moralis';
 import { useQuery } from '@apollo/client';
 import { GET_ACTIVE_NFTS } from '@/gqlqueries/activeNfts.query';
 import NFTBox, { GraphQLNft } from '@/components/NFTBox/NFTBox';
+import DevMarketplaceAbi from '../../abis/DevMarketplace.json';
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+
+const marketplaceContract = process.env.NEXT_PUBLIC_MARKETPLACE_CONTRACT ?? '';
 
 export default function Home() {
-	const { isWeb3Enabled } = useMoralis();
+	const { isWeb3Enabled, account } = useMoralis();
 	const { loading, data: activeNfts } = useQuery(GET_ACTIVE_NFTS);
+	const [benefits, setBenefits] = useState('');
+	const { runContractFunction: getBenefits } = useWeb3Contract({
+		abi: DevMarketplaceAbi,
+		contractAddress: marketplaceContract,
+		functionName: 'getBenefits',
+		params: {
+			seller: account,
+		},
+	});
+
+	const { runContractFunction: withdraw } = useWeb3Contract({
+		abi: DevMarketplaceAbi,
+		contractAddress: marketplaceContract,
+		functionName: 'withdraw',
+	});
+
+	function handleWithdraw() {
+		withdraw({
+			onSuccess: () => {
+				console.log('Withdrew all');
+			},
+			onError: (err) => {
+				console.warn('failed to widht', err);
+			},
+		});
+	}
+
+	useEffect(() => {
+		getBenefits().then((benefits) => {
+			setBenefits(ethers.formatUnits(`${benefits}`));
+		});
+	}, []);
 
 	return (
 		<main className={styles.main}>
@@ -15,8 +52,8 @@ export default function Home() {
 			{isWeb3Enabled && (
 				<div>
 					<div>
-						<h3>Benefits</h3>
-						<button>Withdraw</button>
+						<h3>Benefits: {benefits} ETH</h3>
+						<button onClick={handleWithdraw}>Withdraw</button>
 					</div>
 					{loading ? (
 						<span>Fetching...</span>
