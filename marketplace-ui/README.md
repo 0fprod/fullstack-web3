c8777032ab719d1f69a1569f2fde40d6a50bfce0
franjpr <franpr.dev@protonmail.com>
2023-02-22 17:55:17 +0000

    feat: implement lists owned nfts

--- marketplace-ui/README.md
+++ marketplace-ui/README.md
@@ -1,8 +1,8 @@
-1. Page where user lists its owned nfts
+1. Page where user lists its owned nfts ✅
    1. Can put them on sale
    2. Can update their price
    3. Can remove them from store
 2. Page where user can see active nfts ✅
-   1. Can buy nfts
+   1. Can buy nfts ✅
    2. Can see/withdraw benefis ✅
 3. Navbar where user can see balance and connect / disconnect btn ✅
--- marketplace-ui/src/pages/my-nfts.tsx
+++ marketplace-ui/src/pages/my-nfts.tsx
@@ -1,3 +1,38 @@
+import { INftCardProps, NftCard, useNotification } from '@web3uikit/core';
+import { useCallback, useEffect, useState } from 'react';
+import { useMoralis } from 'react-moralis';
+
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY ?? '';
+
 export default function MyNfts() {
-	return <>Mynft</>;
+	const { account, isWeb3Enabled } = useMoralis();
+	const dispatch = useNotification();
+	const [nfts, setNFTS] = useState([]);
+
+	const fetchNftsByAddress = useCallback(async () => {
+		const d = await fetch(`https://deep-index.moralis.io/api/v2/${account}/nft?chain=goerli`, {
+			method: 'GET',
+			headers: {
+				'X-API-KEY': API_KEY,
+			},
+		});
+		return await d.json();
+	}, []);
+
+	useEffect(() => {
+		if (account != null && fetchNftsByAddress) {
+			fetchNftsByAddress().then((response) => {
+				setNFTS(response.result);
+			});
+		}
+	}, [account, fetchNftsByAddress]);
+
+	return (
+		<>
+			<h2>Owned NFTs</h2>
+			{isWeb3Enabled
+				? nfts.map((nft: INftCardProps['moralisApiResult']) => <NftCard chain="goerli" moralisApiResult={nft} key={`${nft.token_address}${nft.token_id}`} />)
+				: 'Please connect a Wallet'}
+		</>
+	);
 }
